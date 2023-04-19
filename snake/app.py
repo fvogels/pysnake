0bab51d256e3ae75b373330c1c3ac89482234124
Frederic Vogels <frederic.vogels@ucll.be>
2023-04-19 23:22:54 +0200

    Level class

--- snake/app.py
+++ snake/app.py
@@ -3,7 +3,6 @@ import sys
 
 
 FRAMES_PER_SECOND = 75
-GRID_SIZE = (32, 32)
 CELL_SIZE = 32
 
 BLACK = pygame.Color(0, 0, 0)
@@ -32,32 +31,43 @@ class SnakeSegment:
         return RED
 
 
-def create_display_surface():
-    grid_width, grid_height = GRID_SIZE
-    surface_width = grid_width * CELL_SIZE
-    surface_height = grid_height * CELL_SIZE
-    surface_size = (surface_width, surface_height)
+class Level:
+    def __init__(self):
+        self.__grid = self.__create_grid()
+
+    @property
+    def width(self):
+        return len(self.__grid[0])
+
+    @property
+    def height(self):
+        return len(self.__grid)
+
+    def __create_grid(self):
+        width, height = (32, 32)
+        grid = [
+            [Empty() for _ in range(width)]
+            for _ in range(height)
+        ]
+        for x in range(width):
+            grid[0][x] = grid[height-1][x] = Wall()
+        for y in range(height):
+            grid[y][0] = grid[y][width-1] = Wall()
+        return grid
+
+    def __getitem__(self, position):
+        x, y = position
+        return self.__grid[y][x]
+
+
+def create_display_surface(surface_size):
     return pygame.display.set_mode(surface_size)
 
 
-def create_grid():
-    width, height = GRID_SIZE
-    grid = [
-        [Empty() for _ in range(width)]
-        for _ in range(height)
-    ]
-    for x in range(width):
-        grid[0][x] = grid[height-1][x] = Wall()
-    for y in range(height):
-        grid[y][0] = grid[y][width-1] = Wall()
-    return grid
-
-
 def render_grid(surface, grid):
-    width, height = GRID_SIZE
-    for y in range(height):
-        for x in range(width):
-            cell = grid[y][x]
+    for y in range(grid.height):
+        for x in range(grid.width):
+            cell = grid[(x, y)]
             color = cell.color
             rectangle = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
             pygame.draw.rect(surface, color, rectangle)
@@ -67,9 +77,9 @@ def render_grid(surface, grid):
 pygame.init()
 
 # Create window with given size
-display_surface = create_display_surface()
+grid = Level()
+display_surface = create_display_surface((grid.width * CELL_SIZE, grid.height * CELL_SIZE))
 clock = pygame.time.Clock()
-grid = create_grid()
 
 
 while True:
